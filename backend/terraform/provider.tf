6c4e0cef844448961a53fa5d3396cb94003cdceb
robgparedes <robertson.paredes@gmail.com>
2026-03-19 20:53:16 +0800

    Switch Terraform backend to S3 lockfile

--- backend/terraform/provider.tf
+++ backend/terraform/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "robgparedes-cloudresume-tfstate-490058394460-ap-southeast-2-an"
+    key            = "backend/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "cloudresume-terraform-locks"
+    use_lockfile   = true
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
